868c574e8132d66093e38bec3e2586cc91f1d3de
iceApp <worktest2000@gmail.com>
2020-04-21 01:52:26 +0900

    Add dataClass

---
+++ app/src/main/java/com/example/meditation/ThemeData.kt
@@ -0,0 +1,9 @@
+package com.example.meditation
+
+data class ThemeData(
+    val themeId: Int,
+    val themeNameResId: Int,
+    val themeLandPicResId: Int,
+    val themeSpPicResId: Int,
+    val themeSoundId: Int
+)
