7f06e353e9bc68d85355e3515c4b4aa23184d335
iceApp <worktest2000@gmail.com>
2020-04-28 00:40:41 +0900

    Add  DataBinding

--- app/src/main/java/com/example/meditation/MainFragment.kt
+++ app/src/main/java/com/example/meditation/MainFragment.kt
@@ -7,12 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import kotlinx.android.synthetic.main.fragment_main.*
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import com.example.meditation.databinding.FragmentMainBinding
 import net.minpro.meditation.PlayStatus
 
 /**
@@ -21,41 +23,49 @@ import net.minpro.meditation.PlayStatus
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false)
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        binding.apply {
+            viewmodel = viewModel
+            lifecycleOwner = activity
+        }
         viewModel.initParameters()
         observeViewModel()
     }
 
     private fun observeViewModel() {
-        viewModel.txtLevel.observe(viewLifecycleOwner, Observer { levelTxt ->
-            txtLevel.text = levelTxt
-        })
-
-        viewModel.txtTheme.observe(viewLifecycleOwner, Observer { themeTxt ->
-            txtTheme.text = themeTxt
-        })
-
-        viewModel.displayTimeSeconds.observe(viewLifecycleOwner, Observer { displayTime ->
-            txtTime.text = displayTime
-        })
-
-        viewModel.msgUpperSmall.observe(viewLifecycleOwner, Observer { upperTxt ->
-            txtMsgUpperSmall.text = upperTxt
-        })
-
-        viewModel.msgLowerLarge.observe(viewLifecycleOwner, Observer { lowerTxt ->
-            txtMsgLowerLarge.text = lowerTxt
-        })
+//        viewModel.txtLevel.observe(viewLifecycleOwner, Observer { levelTxt ->
+//            txtLevel.text = levelTxt
+//        })
+//
+//        viewModel.txtTheme.observe(viewLifecycleOwner, Observer { themeTxt ->
+//            txtTheme.text = themeTxt
+//        })
+//
+//        viewModel.displayTimeSeconds.observe(viewLifecycleOwner, Observer { displayTime ->
+//            txtTime.text = displayTime
+//        })
+//
+//        viewModel.msgUpperSmall.observe(viewLifecycleOwner, Observer { upperTxt ->
+//            txtMsgUpperSmall.text = upperTxt
+//        })
+//
+//        viewModel.msgLowerLarge.observe(viewLifecycleOwner, Observer { lowerTxt ->
+//            txtMsgLowerLarge.text = lowerTxt
+//        })
 
         viewModel.playStatus.observe(viewLifecycleOwner, Observer { status ->
             when (status){
